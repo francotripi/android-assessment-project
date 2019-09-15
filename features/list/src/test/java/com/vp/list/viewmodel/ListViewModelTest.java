f00e3b85f201142b81958b08763cfd58b1d6fc49
Franco Tripi <ftripi@rccl.com>
2019-09-15 10:07:41 -0300

    1. The wrong state: fixes List view and implements unit test.

--- features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
+++ features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
@@ -54,4 +54,24 @@ public class ListViewModelTest {
         verify(mockObserver).onChanged(SearchResult.inProgress());
     }
 
+    @Test
+    public void shouldReturnSuccessState() {
+        //given
+        SearchService searchService = mock(SearchService.class);
+        when(searchService.search(anyString(), anyInt())).thenReturn(Calls.response(mock(SearchResponse.class)));
+        ListViewModel listViewModel = new ListViewModel(searchService);
+        Observer<SearchResult> mockObserver = (Observer<SearchResult>) mock(Observer.class);
+        listViewModel.observeMovies().observeForever(mockObserver);
+
+        //when
+        listViewModel.searchMoviesByTitle("title", 1);
+
+        //then
+        verify(mockObserver).onChanged(
+                SearchResult.success(
+                        listViewModel.observeMovies().getValue().getItems(),
+                        listViewModel.observeMovies().getValue().getTotalResult()
+                )
+        );
+    }
 }
